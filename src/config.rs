fdfca9b281e1e5bb3b9b1992191034295104b500
Хлебников Андрей <andrei.khlebnikov@domrf.ru>
2021-02-19 10:15:21 +0300

    Первия версия формирования конфигурации, вывод в стандартный поток

--- src/config.rs
+++ src/config.rs
@@ -6,6 +6,7 @@ use std::fs::File;
 use serde_derive::{Deserialize, Serialize};
 use serde_json::from_str;
 
+#[derive(Clone)]
 pub struct Config {
     pub address: String,
     pub port: u16,
@@ -15,6 +16,7 @@ pub struct Config {
     pub users: HashMap<String, User>,
 }
 
+#[derive(Clone)]
 pub struct User {
     pub name: String,
     pub ip: String,
@@ -22,6 +24,7 @@ pub struct User {
     pub security_group: Vec<String>,
 }
 
+#[derive(Clone)]
 pub struct SecurityGroup {
     pub name: String,
     pub allowed_ip: Vec<String>,
@@ -113,6 +116,23 @@ impl ToString for Config {
         buf.push_str(format!("Address = {}\n", self.address.clone()).as_str());
         buf.push_str(format!("ListenPort = {}\n", self.port).as_str());
         buf.push_str(format!("PrivateKey = {}\n", self.private_key).as_str());
+        self.users.iter().for_each(|u| {
+            let sg = self.security_groups.clone();
+            u.1.security_group.clone().iter().for_each(|g| {
+                let go = sg.get(g).expect("SecurityGroup not defined");
+                go.allowed_ip.iter().for_each(|ai| {
+                    buf.push_str(format!("PostUp = iptables -I INPUT -s {} -d {} -j ACCEPT\n", u.1.ip, ai).as_str());
+                    buf.push_str(format!("PostDown = iptables -D INPUT -s {} -d {} -j ACCEPT\n", u.1.ip, ai).as_str());
+                });
+            })
+        });
+
+        self.users.iter().for_each(|u| {
+            buf.push_str(format!("\n").as_str());
+            buf.push_str(format!("[Peer] #{}\n", u.0).as_str());
+            buf.push_str(format!("PublicKey = {}\n", u.1.public_key).as_str());
+            buf.push_str(format!("AllowedIPs = {}\n", u.1.ip).as_str());
+        });
         buf
     }
 }
